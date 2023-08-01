929475c198e9157bfbd8bbf27640e30ecc071299
mgacoder <miguelgalvanalvarado@gmail.com>
2023-07-31 22:51:08 -0700

    finished list fragment layout

--- app/src/main/java/com/example/mytodos/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/mytodos/fragments/add/AddFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.todoapp.R
+import com.example.mytodos.R
 
 class AddFragment : Fragment() {
 
