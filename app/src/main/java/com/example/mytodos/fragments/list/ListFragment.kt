5209ef4ba8a45e22a11e98c67f155ec9d5bdafcc
mgacoder <miguelgalvanalvarado@gmail.com>
2023-08-01 01:05:47 -0700

    finished add fragment layout

--- app/src/main/java/com/example/mytodos/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/mytodos/fragments/list/ListFragment.kt
@@ -5,16 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.mytodos.R
+import com.example.mytodos.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+    private lateinit var binding: FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
-    }
+        binding = FragmentListBinding.inflate(inflater, container, false)
 
+        binding.fragmentListFloatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return binding.root
+    }
 }
