bca24d914a59a3f61fa83d391d3627bde15f9010
mgacoder <miguelgalvanalvarado@gmail.com>
2023-08-02 20:19:04 -0700

    created menu for list fragment

--- app/src/main/java/com/example/mytodos/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/mytodos/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.example.mytodos.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.mytodos.R
 import com.example.mytodos.databinding.FragmentListBinding
@@ -28,6 +26,13 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    @Deprecated("")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 }
