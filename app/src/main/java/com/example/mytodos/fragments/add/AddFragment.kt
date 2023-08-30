39fe1a9022447446e5c0f165996b8fca58e47481
mgacoder <miguelgalvanalvarado@gmail.com>
2023-08-30 16:16:08 -0700

    added share view model and priority text color

--- app/src/main/java/com/example/mytodos/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/mytodos/fragments/add/AddFragment.kt
@@ -13,10 +13,12 @@ import com.example.mytodos.R
 import com.example.mytodos.data.models.Priority
 import com.example.mytodos.data.models.ToDoData
 import com.example.mytodos.data.viewmodel.ToDoViewModel
+import com.example.mytodos.fragments.SharedViewModel
 
 class AddFragment : Fragment() {
 
     private val mToDoViewModel: ToDoViewModel by viewModels()
+    private val mSharedViewModel: SharedViewModel by viewModels()
     private lateinit var mTitle: EditText
     private lateinit var mPriority: Spinner
     private lateinit var mDescription: EditText
@@ -27,9 +29,9 @@ class AddFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_add, container, false)
-        mTitle = view.findViewById(R.id.title_editText)
-        mPriority = view.findViewById(R.id.priority_spinner)
-        mDescription = view.findViewById(R.id.description_editTextTextMultiLine)
+        mTitle = view.findViewById(R.id.title_editText) as EditText
+        mPriority = view.findViewById(R.id.priority_spinner) as Spinner
+        mDescription = view.findViewById(R.id.description_editTextTextMultiLine) as EditText
         // Set menu
         setHasOptionsMenu(true)
         return view
@@ -50,35 +52,20 @@ class AddFragment : Fragment() {
         val title = mTitle.toString()
         val description = mDescription.toString()
         val priority = mPriority.toString()
-        val validation = verifyDataFromUser(title, description)
+        val validation = mSharedViewModel.verifyDataFromUser(title, description)
         if(validation) {
             val newData = ToDoData(
                 0,
                 title,
-                parsePriority(priority),
+                mSharedViewModel.parsePriority(priority),
                 description
             )
             mToDoViewModel.insertData(newData)
             Toast.makeText(context, "Successfully added", Toast.LENGTH_SHORT).show()
             // Navigate Back
             findNavController().navigate(R.id.action_addFragment_to_listFragment)
-        }
-    }
-
-    private fun parsePriority(priority: String): Priority {
-        return when(priority) {
-            "High Priority" -> {Priority.HIGH}
-            "Medium Priority" -> {Priority.MEDIUM}
-            "Low Priority" -> {Priority.LOW}
-            else -> Priority.LOW
-        }
-    }
-
-    private fun verifyDataFromUser(title: String, description: String): Boolean {
-        return if(TextUtils.isEmpty(title) || TextUtils.isEmpty(description)) {
-            false
         } else {
-            !(title.isEmpty() || description.isEmpty())
+            Toast.makeText(context, "Add unsuccessful", Toast.LENGTH_SHORT).show()
         }
     }
 }
