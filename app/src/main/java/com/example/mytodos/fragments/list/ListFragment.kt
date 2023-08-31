11d786708419d5380f4ce312cc15f693b3c01558
mgacoder <miguelgalvanalvarado@gmail.com>
2023-08-30 22:18:22 -0700

    display data in recycler view

--- app/src/main/java/com/example/mytodos/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/mytodos/fragments/list/ListFragment.kt
@@ -3,13 +3,19 @@ package com.example.mytodos.fragments.list
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mytodos.R
+import com.example.mytodos.data.viewmodel.ToDoViewModel
 import com.example.mytodos.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
     private lateinit var binding: FragmentListBinding
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+    private val adapter: ListAdapter by lazy { ListAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +23,14 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentListBinding.inflate(inflater, container, false)
+
+        val recyclerView = binding.fragmentListRecyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner, Observer { data ->
+            adapter.setData(data)
+        })
 
         binding.fragmentListFloatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
