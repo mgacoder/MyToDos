7f1c17003fbb206e17331fa8c8130d25651d1f99
mgacoder <miguelgalvanalvarado@gmail.com>
2023-09-10 00:27:25 -0700

    enabled sort by priority for recyclerview list

--- app/src/main/java/com/example/mytodos/data/repository/ToDoRepository.kt
+++ app/src/main/java/com/example/mytodos/data/repository/ToDoRepository.kt
@@ -7,6 +7,8 @@ import com.example.mytodos.data.models.ToDoData
 class ToDoRepository(private val toDoDao: ToDoDao) {
 
     val getAllData: LiveData<List<ToDoData>> = toDoDao.getAllData()
+    val sortByHighPriority: LiveData<List<ToDoData>> = toDoDao.sortByHighPriority()
+    val sortByLowPriority: LiveData<List<ToDoData>> = toDoDao.sortByLowPriority()
 
     suspend fun insertData(todoData: ToDoData) {
         toDoDao.insertData(todoData)
